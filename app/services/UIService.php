1c08c4ed7ff6771d1223b4fdf5ec3d6a26b54692
Felisky <62891363+Felisky@users.noreply.github.com>
2020-12-03 11:33:06 +0100

    Create UIService.php

---
+++ app/services/UIService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace services;
+
+use Ajax\php\ubiquity\JsUtils;
+use Ajax\service\JArray;
+use Ubiquity\orm\DAO;
+use models\Question;
+use models\Typeq;
+use models\User;
+
+class UIService {
+	protected $jquery;
+	protected $semantic;
+	public function __construct(JsUtils $jq) {
+		$this->jquery = $jq;
+		$this->semantic = $jq->semantic ();
+	}
+	public function questionForm() {
+		$q = new Question ();
+		$frm = $this->jquery->semantic ()->dataForm ( 'form', $q );
+		$frm->setFields ( [ 
+				'caption',
+				'typeq'
+		] );
+	//	$types = DAO::getAll ( Typeq::class );
+		$q->setTypeq ( current ( $types ) );
+		$frm->fieldAsDropDown ( 'typeq', JArray::modelArray ( $types, 'getId' ) );
+		return $frm;
+	}
+	public function userForm() {
+		$frm = $this->jquery->semantic ()->dataForm ( 'form', new User () );
+		$frm->setFields ( [ 
+				'id',
+				'lastname',
+				'email',
+				'firstname',
+				'submit'
+		] );
+		$frm->fieldAsInput ( 'lastname', [ 
+				'rules' => [ 
+						'empty',
+						'length[10]'
+				]
+		] );
+		$frm->fieldAsInput ( 'email', [ 
+				'inputType' => 'email',
+				'rules' => [ 
+						[ 
+								'email',
+								'Valeur {value} invalide !'
+						]
+				]
+		] );
+		$frm->setValidationParams ( [ 
+				"on" => "blur",
+				"inline" => true
+		] );
+		return $frm;
+	}
+}
