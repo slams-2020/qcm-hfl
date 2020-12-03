53af35682e34a2aeffa4d23f6d5cc1fa187f4377
Felisky <62891363+Felisky@users.noreply.github.com>
2020-12-03 11:55:50 +0100

    update models 2

---
+++ app/models/qcm/Exam.php
@@ -0,0 +1,121 @@
+<?php
+namespace models\qcm;
+/**
+ * @database('qcm')
+*/
+/**
+ * @table('exam')
+*/
+class Exam{
+	/**
+	 * @id
+	 * @column("name"=>"id","nullable"=>false,"dbType"=>"int(11)")
+	 * @validator("id","constraints"=>array("autoinc"=>true))
+	*/
+	private $id;
+
+	/**
+	 * @column("name"=>"dated","nullable"=>true,"dbType"=>"datetime")
+	 * @validator("type","dateTime")
+	 * @transformer("name"=>"datetime")
+	*/
+	private $dated;
+
+	/**
+	 * @column("name"=>"datef","nullable"=>true,"dbType"=>"datetime")
+	 * @validator("type","dateTime")
+	 * @transformer("name"=>"datetime")
+	*/
+	private $datef;
+
+	/**
+	 * @column("name"=>"status","nullable"=>true,"dbType"=>"varchar(42)")
+	 * @validator("length","constraints"=>array("max"=>42))
+	*/
+	private $status;
+
+	/**
+	 * @oneToMany("mappedBy"=>"exam","className"=>"models\\qcm\\Examoption")
+	*/
+	private $examoptions;
+
+	/**
+	 * @oneToMany("mappedBy"=>"exam","className"=>"models\\qcm\\Qcm")
+	*/
+	private $qcms;
+
+	/**
+	 * @manyToOne
+	 * @joinColumn("className"=>"models\\qcm\\Group","name"=>"idGroup","nullable"=>false)
+	*/
+	private $group;
+
+	 public function getId(){
+		return $this->id;
+	}
+
+	 public function setId($id){
+		$this->id=$id;
+	}
+
+	 public function getDated(){
+		return $this->dated;
+	}
+
+	 public function setDated($dated){
+		$this->dated=$dated;
+	}
+
+	 public function getDatef(){
+		return $this->datef;
+	}
+
+	 public function setDatef($datef){
+		$this->datef=$datef;
+	}
+
+	 public function getStatus(){
+		return $this->status;
+	}
+
+	 public function setStatus($status){
+		$this->status=$status;
+	}
+
+	 public function getExamoptions(){
+		return $this->examoptions;
+	}
+
+	 public function setExamoptions($examoptions){
+		$this->examoptions=$examoptions;
+	}
+
+	 public function addExamoption($examoption){
+		$this->examoptions[]=$examoption;
+	}
+
+	 public function getQcms(){
+		return $this->qcms;
+	}
+
+	 public function setQcms($qcms){
+		$this->qcms=$qcms;
+	}
+
+	 public function addQcm($qcm){
+		$this->qcms[]=$qcm;
+	}
+
+	 public function getGroup(){
+		return $this->group;
+	}
+
+	 public function setGroup($group){
+		$this->group=$group;
+	}
+
+	 public function __toString(){
+		return $this->id.'';
+	}
+
+}
---
+++ app/models/qcm/Useranswer.php
@@ -0,0 +1,101 @@
+<?php
+namespace models\qcm;
+/**
+ * @database('qcm')
+*/
+/**
+ * @table('useranswer')
+*/
+class Useranswer{
+	/**
+	 * @id
+	 * @column("name"=>"idUser","nullable"=>false,"dbType"=>"int(11)")
+	 * @validator("id","constraints"=>array("autoinc"=>true))
+	*/
+	private $idUser;
+
+	/**
+	 * @id
+	 * @column("name"=>"idAnswer","nullable"=>false,"dbType"=>"int(11)")
+	 * @validator("id","constraints"=>array("autoinc"=>true))
+	*/
+	private $idAnswer;
+
+	/**
+	 * @id
+	 * @column("name"=>"idQcm","nullable"=>false,"dbType"=>"int(11)")
+	 * @validator("id","constraints"=>array("autoinc"=>true))
+	*/
+	private $idQcm;
+
+	/**
+	 * @manyToOne
+	 * @joinColumn("className"=>"models\\qcm\\Answer","name"=>"idAnswer","nullable"=>false)
+	*/
+	private $answer;
+
+	/**
+	 * @manyToOne
+	 * @joinColumn("className"=>"models\\qcm\\Qcm","name"=>"idQcm","nullable"=>false)
+	*/
+	private $qcm;
+
+	/**
+	 * @manyToOne
+	 * @joinColumn("className"=>"models\\qcm\\User","name"=>"idUser","nullable"=>false)
+	*/
+	private $user;
+
+	 public function getIdUser(){
+		return $this->idUser;
+	}
+
+	 public function setIdUser($idUser){
+		$this->idUser=$idUser;
+	}
+
+	 public function getIdAnswer(){
+		return $this->idAnswer;
+	}
+
+	 public function setIdAnswer($idAnswer){
+		$this->idAnswer=$idAnswer;
+	}
+
+	 public function getIdQcm(){
+		return $this->idQcm;
+	}
+
+	 public function setIdQcm($idQcm){
+		$this->idQcm=$idQcm;
+	}
+
+	 public function getAnswer(){
+		return $this->answer;
+	}
+
+	 public function setAnswer($answer){
+		$this->answer=$answer;
+	}
+
+	 public function getQcm(){
+		return $this->qcm;
+	}
+
+	 public function setQcm($qcm){
+		$this->qcm=$qcm;
+	}
+
+	 public function getUser(){
+		return $this->user;
+	}
+
+	 public function setUser($user){
+		$this->user=$user;
+	}
+
+	 public function __toString(){
+		return $this->idUser.'';
+	}
+
+}
