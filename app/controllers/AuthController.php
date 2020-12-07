bb93fedb20df513abaf47d8369ba1469213b1cfc
Freaky5 <56235179+Freaky5@users.noreply.github.com>
2020-12-07 11:09:43 +0100

    Create AuthController.php

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+namespace controllers;
+use Ubiquity\utils\http\USession;
+use Ubiquity\utils\http\URequest;
+
+ /**
+  * Auth Controller AuthController
+  */
+class AuthController extends \Ubiquity\controllers\auth\AuthController{
+
+	protected function onConnect($connected) {
+		$urlParts=$this->getOriginalURL();
+		USession::set($this->_getUserSessionKey(), $connected);
+		if(isset($urlParts)){
+			$this->_forward(implode("/",$urlParts));
+		}else{
+			//TODO
+			//Forwarding to the default controller/action
+		}
+	}
+
+	protected function _connect() {
+		if(URequest::isPost()){
+			$email=URequest::post($this->_getLoginInputName());
+			$password=URequest::post($this->_getPasswordInputName());
+			//TODO
+			//Loading from the database the user corresponding to the parameters
+			//Checking user creditentials
+			//Returning the user
+		}
+		return;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \Ubiquity\controllers\auth\AuthController::isValidUser()
+	 */
+	public function _isValidUser($action=null) {
+		return USession::exists($this->_getUserSessionKey());
+	}
+
+	public function _getBaseRoute() {
+		return 'AuthController';
+	}
+	
+
+
+}
