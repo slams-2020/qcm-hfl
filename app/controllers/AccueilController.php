e27a6c8310545db92e26a54d3362e31e78cf35b5
Felisky <62891363+Felisky@users.noreply.github.com>
2020-12-10 11:55:12 +0100

    navbar semantic

--- app/controllers/AccueilController.php
+++ app/controllers/AccueilController.php
@@ -2,10 +2,12 @@
 namespace controllers;
  /**
   * Controller AccueilController
+  * @property \Ajax\php\ubiquity\JsUtils $jquery
   */
 class AccueilController extends ControllerBase{
 
 	public function index(){
-		$this->loadView("AccueilController/index.html");
+	    $this->jquery->exec('$(".dropdown").dropdown();',true);
+		$this->jquery->renderView("AccueilController/index.html");
 	}
 }
