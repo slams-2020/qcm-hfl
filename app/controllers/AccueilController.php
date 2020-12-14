e9ebb185b0deeedc5b9d6944b26eeca855f719e5
Felisky <62891363+Felisky@users.noreply.github.com>
2020-12-14 09:39:41 +0100

    Graphic

--- app/controllers/AccueilController.php
+++ app/controllers/AccueilController.php
@@ -11,3 +11,9 @@ class AccueilController extends ControllerBase{
 		$this->jquery->renderView("AccueilController/index.html");
 	}
 }
+
+//$ref = $_SERVER['HTTP_REFERER'];
+//if($ref == 'http://127.0.0.1:8090/AccueilController/index') {
+//    echo 'good';
+//}
+?>
