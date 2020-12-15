dfbf303c6312bc145965a0f71a68b65a59f82589
Felisky <62891363+Felisky@users.noreply.github.com>
2020-12-15 16:26:08 +0100

    connect

--- app/controllers/AccueilController.php
+++ app/controllers/AccueilController.php
@@ -1,5 +1,8 @@
 <?php
 namespace controllers;
+ use Ubiquity\controllers\Router;
+use Ubiquity\utils\http\USession;
+
  /**
   * Controller AccueilController
   * @property \Ajax\php\ubiquity\JsUtils $jquery
@@ -7,13 +10,18 @@ namespace controllers;
 class AccueilController extends ControllerBase{
 
 	public function index(){
+	    if(USession::get('activeUser')==null){
+	        $user='';
+	    }
+	    else{
+	        $user=USession::get('activeUser')['pseudo'];
+	    }
+	    $this->jquery->postFormOnClick('#loginButton',Router::path('loginPost'),'loginForm');
 	    $this->jquery->exec('$(".dropdown").dropdown();',true);
-		$this->jquery->renderView("AccueilController/index.html");
+		$this->jquery->renderView("AccueilController/index.html",['nom'=>$user]);
 	}
 }
 
-//$ref = $_SERVER['HTTP_REFERER'];
-//if($ref == 'http://127.0.0.1:8090/AccueilController/index') {
-//    echo 'good';
-//}
+
+
 ?>
